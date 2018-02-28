708bd78005d17f661014223b44dd95878ab148ff
Steven Moreland <smoreland@google.com>
2018-02-27 17:00:06 -0800

    davinci: Only include bitness of hal impls that is used.
    Bug: 73829928
    Test: boot, check directories, no errors during boot
    Change-Id: Ia1272b2bcc5006fa75a50631861fbaccdeed7bee

--- device.mk
+++ device.mk
@@ -75,7 +75,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensor
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
 # Wi-Fi
