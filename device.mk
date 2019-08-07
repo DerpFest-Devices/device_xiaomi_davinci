8c9a3fbe9755a52d66446e2565f5fcca0a2a2b34
Demon000 <demonsingur@gmail.com>
2019-08-07 22:51:05 +0200

    davinci: add wifi mac generator
    The default qcacld-3.0 implementation looks for the MAC
    in a firmware with the name `wlan/qca_cld/wlan_mac.bin`.
    That firmware is, by default, symlinked to
    `/mnt/vendor/persist/wlan_mac.bin`.
    Xiaomi has somewhat changed the qcacld-3.0 logic to load
    the MAC from another place. Xiaomi's nvmac puts a plain-text
    MAC in `/data/vendor/mac_addr/wlan_mac.bin`.
    Add a script to write the MAC address found in the file
    created by nvmac to the file in persist that the firmware
    file is symlinked to.
    
    This implementation is (loosely) based on the one
    written for OnePlus 7.
    
    Improved and adapted for davinci by @PIPIPIG233666
    
    Co-authored-by: PIPIPIG233666 <2212848813@qq.com>
    Change-Id: I8e22ddbc8a8b6b5f493a32ab179e48c1815e19bc
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- device.mk
+++ device.mk
@@ -85,3 +85,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
+# WiFi
+PRODUCT_PACKAGES += \
+    wifi-mac-generator
