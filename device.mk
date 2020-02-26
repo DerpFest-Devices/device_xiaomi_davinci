ac76648463dc77d4298d04a53dc08529baecaebf
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    davinci: Build and enable fastbootd
    Since OEM fastboot implementation does not allow us to flash
    critical partitions, fastbootd is useful for upgrading firmware.
    
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -43,6 +43,10 @@ PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fstab
 PRODUCT_PACKAGES += \
     fstab.qcom \
