c5a2a1a39533abfc3e930032029b714100248b44
DarkDampSquib <andrin.geiger1998@gmail.com>
2020-08-03 20:17:52 +0200

    davinci: Include MiuiCamera
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- derp_davinci.mk
+++ derp_davinci.mk
@@ -37,3 +37,6 @@ PRODUCT_MODEL := Mi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
