56fe90206ee8ccf5ea6ee2204744f6b5d5dfa601
DarkDampSquib <andrin.geiger1998@gmail.com>
2020-08-11 12:16:58 +0200

    Revert "davinci: Build livedisplay"
    This reverts commit 8f9c6a02fc91d5fdc2c2183cfcb34d52db84a6b9.
    
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- device.mk
+++ device.mk
@@ -212,10 +212,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.davinci
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
