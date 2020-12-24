065aeaba809ca12c0778ce5405449cc3c743d522
adi8900 <adrianszymanski242@gmail.com>
2020-12-24 13:07:09 +0100

    davinci: Go official
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- derp_davinci.mk
+++ derp_davinci.mk
@@ -9,6 +9,7 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
+DERP_BUILDTYPE := Official
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_davinci
