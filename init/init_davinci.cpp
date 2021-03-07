b5d6c69a21ae5c799b3e34ba3df8f2f0489de121
adi8900 <adrianszymanski242@gmail.com>
2021-03-07 12:26:32 +0100

    davinci: Update to march redfin fingerprint
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -62,11 +62,11 @@ constexpr const char *BUILD_DESCRIPTION[] = {
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/"
+    "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/"
     "release-keys",
-    "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/"
+    "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/"
     "release-keys",
-    "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/"
+    "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/"
     "release-keys",
 };
 
