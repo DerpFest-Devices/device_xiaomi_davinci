a9bd40a4fee9b2cf0ab89bfb1bfb129aac31badf
Omar Hamad <etahamad@icloud.com>
2021-05-04 05:30:44 +0200

    davinci: Update fingerprint to Redfin
    * RQ2A.210505.003
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -62,11 +62,11 @@ constexpr const char *BUILD_DESCRIPTION[] = {
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/"
+    "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/"
     "release-keys",
-    "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/"
+    "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/"
     "release-keys",
-    "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/"
+    "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/"
     "release-keys",
 };
 
