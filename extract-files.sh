8d8aa1cfd1ab8a35e4dd58f613014e6937b34f84
adi8900 <adrianszymanski242@gmail.com>
2021-06-03 19:58:02 +0200

    davinci: Move camera.qcom.so patching into common [1/2]
    * camxoverridesettings.txt also being patching by common tree extract-files.sh and it works
    * having it in davinci tree just doesn't patch the lib as should from megvii deps, it is annoying for patching it manually after using extract-files script.
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -18,14 +18,4 @@ export DEVICE=davinci
 export DEVICE_COMMON=sm6150-common
 export VENDOR=xiaomi
 
-function blob_fixup() {
-    case "${1}" in
-    vendor/lib64/hw/camera.qcom.so)
-        "${PATCHELF}" --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
-        "${PATCHELF}" --remove-needed "libmegface.so" "${2}"
-        "${PATCHELF}" --add-needed "libshim_megvii.so" "${2}"
-        ;;
-    esac
-}
-
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
