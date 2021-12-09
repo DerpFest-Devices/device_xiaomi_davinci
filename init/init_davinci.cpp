40b63ab4007449be40f57e0de6e4ec70d84914aa
adi8900 <adrianszymanski242@gmail.com>
2021-12-09 17:46:06 +0100

    davinci: Update China stable build description
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -56,7 +56,7 @@ constexpr const char *DEVICES[] = {
 };
 
 constexpr const char *BUILD_DESCRIPTION[] = {
-    "davinci-user 11 RKQ1.200826.002 V12.5.1.0.RFJCNXM release-keys",
+    "davinci-user 11 RKQ1.200826.002 V12.5.2.0.RFJCNXM release-keys",
     "davinci-user 11 RKQ1.200826.002 V12.1.4.0.RFJMIXM release-keys",
     "davinciin-user 11 RKQ1.200826.002 V12.1.4.0.RFJINXM release-keys",
 };
