556e6d50287e506dbb8600777f0c49904d42a154
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    davinci: Support UDFPS on AoD
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1
    
    Build/use common UDFPS sensor
    Change-Id: I7a4ca9a3e98e0af90be5aedc88fa1eaa344abb76
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -60,7 +60,11 @@ PRODUCT_COPY_FILES += \
 # Sensor
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    sensors.udfps
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
