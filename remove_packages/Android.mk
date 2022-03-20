9237a61ddb107491deca9c7bd1763a1e8de3c51f
adi8900 <adrianszymanski242@gmail.com>
2022-03-20 14:43:26 +0100

    davinci: Drop more apps
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube Maps SafetyHubPrebuilt Drive
+LOCAL_OVERRIDES_PACKAGES := YouTube Maps SafetyHubPrebuilt Drive PrebuiltGmail Photos
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
