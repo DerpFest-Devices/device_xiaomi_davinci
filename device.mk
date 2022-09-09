8dfe069a0b979173787f01243f29cd577b5e4746
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-09 14:47:31 +0200

    davinci: Treat retrofit devices as launching with dynamic partitions
    Change-Id: Ib0dfe4ee3da4c583d924d85a79d5ec37a2f9ba87

--- device.mk
+++ device.mk
@@ -38,7 +38,6 @@ PRODUCT_PACKAGES += \
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
