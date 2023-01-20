988b10b196836a054db6a303d3ddbe898cf3ba3e
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-20 17:02:52 +0000

    davinci: Retrofit Dynamic Partitions
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 
-# Fstab
+# Fstabs
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
