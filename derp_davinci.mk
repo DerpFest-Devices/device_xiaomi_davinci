b6bf3b7c7a94fbf584072a219e81acfb30bd9b12
adi8900 <adrianszymanski242@gmail.com>
2023-11-05 15:08:32 +0100

    davinci: Udfps animations aren't ported yet to U
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- derp_davinci.mk
+++ derp_davinci.mk
@@ -10,7 +10,6 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 # Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 DERP_BUILDTYPE := Official
-EXTRA_UDFPS_ANIMATIONS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_davinci
