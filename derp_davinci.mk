89707e9c867ec5587cb95cf4335354127c409bef
adi8900 <adrianszymanski242@gmail.com>
2024-03-10 18:06:30 +0100

    davinci: Update UDFPS icons and animations flag
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- derp_davinci.mk
+++ derp_davinci.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 # Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 DERP_BUILDTYPE := Official
-EXTRA_UDFPS_ANIMATIONS := true
+EXTRA_UDFPS_ICONS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_davinci
