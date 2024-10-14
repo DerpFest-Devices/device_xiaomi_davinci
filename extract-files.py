a9ee2e6ad7e1ebfe2ce40cd062be9bd92c546cbd
basamaryan <basam.aryan@gmail.com>
2024-10-13 20:00:11 -0400

    davinci: Switch to py extract utils
    Change-Id: If53e93faec4f455542c9eb46ece28d355e5910e4
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'hardware/qcom-caf/sm8150',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/display',
+    'vendor/xiaomi/sm6150-common',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+module = ExtractUtilsModule(
+    'davinci',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm6150-common', module.vendor
+    )
+    utils.run()
