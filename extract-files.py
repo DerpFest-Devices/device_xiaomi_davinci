bdc0f4f4838c278127b6b6b56bede3c2e64b07cf
LuK1337 <priv.luk@gmail.com>
2024-12-10 23:07:45 +0100

    davinci: Enable elfchecks for camera libs
    Reference: I23190f0b83c9e28aa2624a7575544b0eaf5c83c8
    Change-Id: I18b0b07bf3a435dd449106653e3ca458b4f07b69
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -29,6 +29,21 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
 }
 
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib64/libalAILDC.so', 'vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    ('vendor/lib64/libVDSuperPhotoAPI.so', 'vendor/lib64/libarcsoft_dualcam_refocus_front.so', 'vendor/lib64/libarcsoft_dualcam_refocus_rear_t.so', 'vendor/lib64/libarcsoft_dualcam_refocus_rear_w.so'): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open')
+        .clear_symbol_version('remote_register_buf_attr')
+        .clear_symbol_version('remote_register_buf'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'davinci',
     'xiaomi',
