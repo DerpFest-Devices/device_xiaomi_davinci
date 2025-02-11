94bb825ad8ef3169448febd92ba9ed25d2671ab7
adi8900 <adrianszymanski242@gmail.com>
2025-02-11 12:35:39 +0100

    davinci: DerpFest bringup
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/derp_davinci.mk
--- device.mk
+++ device.mk
@@ -75,8 +75,7 @@ $(foreach sku, CN GLOBAL, \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     DavinciCNSettingsProviderOverlay \
