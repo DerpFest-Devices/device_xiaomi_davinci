ba83ea1cc399cf812b04a04ec2d400def6a191ce
adi8900 <adrianszymanski242@gmail.com>
2025-05-09 17:47:50 +0200

    davinci: Build stable derp
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- lineage_davinci.mk
+++ lineage_davinci.mk
@@ -11,6 +11,7 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 # Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 DERPFEST_BUILD_TYPE := Official
+DERPFEST_BUILD_VARIANT := Stable
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_davinci
