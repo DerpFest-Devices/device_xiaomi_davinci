3706271aa7cd0952d996db6c8643267651dfd987
Bruno Martins <bgcngm@gmail.com>
2025-11-23 21:14:35 +0000

    davinci: motor: Convert for M3E support
    Change-Id: Ib4d1088f5be3fc2b5c2ff90d5cd1b86603551037

--- motor/src/org/lineageos/popupcamera/PopupCameraSettingsFragment.java
+++ motor/src/org/lineageos/popupcamera/PopupCameraSettingsFragment.java
@@ -25,11 +25,12 @@ import android.os.Bundle;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference.OnPreferenceClickListener;
-import androidx.preference.PreferenceFragmentCompat;
+
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 
 import org.lineageos.popupcamera.R;
 
-public class PopupCameraSettingsFragment extends PreferenceFragmentCompat
+public class PopupCameraSettingsFragment extends SettingsBasePreferenceFragment
         implements OnPreferenceChangeListener, OnPreferenceClickListener {
     private Preference mCalibrationPreference;
     private static final String MOTOR_CALIBRATION_KEY = "motor_calibration";
